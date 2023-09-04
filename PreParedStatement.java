07044c1757fcb1bccd79ab71448399c6fabca6fc
Rishitha dilmith <127425850+Rishitha055@users.noreply.github.com>
2023-09-04 20:47:41 +0530

    PreParedStatement

---
+++ PreParedStatement.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Windows
+ */
+class PreParedStatement {
+
+    void setString(int i, int i0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setString(int i, String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    int executeUpdate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
